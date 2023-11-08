d2432ac45e3e5cf321796752a21ab903128497e5
AbuJubaeid <abujubaeidpranto2@gmail.com>
2023-11-08 17:18:13 +0600

    Add files via upload

--- date-creating/App.js
+++ date-creating/App.js
@@ -1,1 +1,26 @@
+import ExpenseItem from './components/ExpenseItem.js';
 
+function App() {
+  const expenses = [
+    {title: 'Car Insurance',
+    price: 290,
+    date:  new Date(21,7,12)},
+
+    {title: 'Book',
+    price: 250,
+    date:  new Date(21,6,12)},
+
+    {title: 'Paper',
+    price: 280,
+    date: new Date(21,5,12)}
+  ]
+  return (
+    <div>
+      <ExpenseItem title={expenses[0].title} price={expenses[0].price} date={expenses[0].date}></ExpenseItem>
+      <ExpenseItem title={expenses[1].title} price={expenses[1].price} date={expenses[1].date}></ExpenseItem>
+      <ExpenseItem title={expenses[2].title} price={expenses[2].price} date={expenses[2].date}></ExpenseItem>
+    </div>
+  );
+}
+
+export default App;
