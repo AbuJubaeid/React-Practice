e5cf7cc1b4d594eea85e7c615591829f0b17efcc
AbuJubaeid <abujubaeidpranto2@gmail.com>
2023-11-08 17:31:24 +0600

    Add files via upload

--- onClick to change value using useState/App.js
+++ onClick to change value using useState/App.js
@@ -1,1 +1,22 @@
+import Expenses from './components/Expenses/Expenses.js';
 
+function App() {
+  const expenses = [
+    {title: 'Car Insurance',
+    price: 290,
+    date:  new Date(21,7,12)},
+
+    {title: 'Book',
+    price: 250,
+    date:  new Date(21,6,12)},
+
+    {title: 'Paper',
+    price: 280,
+    date: new Date(21,5,12)}
+  ]
+  return (
+    <Expenses items={expenses}/>
+  );
+}
+
+export default App;
