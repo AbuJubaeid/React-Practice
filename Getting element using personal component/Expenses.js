c0ac402ecc08ec0cbd42297c9cadc8b79008b0f1
AbuJubaeid <abujubaeidpranto2@gmail.com>
2023-11-08 17:27:15 +0600

    Add files via upload

---
+++ Getting element using personal component/Expenses.js
@@ -0,0 +1,24 @@
+import ExpenseItem from './ExpenseItem.js';
+import './Expenses.css';
+
+function Expenses (props){
+    return (
+        <div className="expenses">
+      <ExpenseItem 
+      title={props.items[0].title} 
+      price={props.items[0].price} 
+      date={props.items[0].date}/>
+
+      <ExpenseItem 
+      title={props.items[1].title} 
+      price={props.items[1].price} 
+      date={props.items[1].date}/>
+
+      <ExpenseItem 
+      title={props.items[2].title} 
+      price={props.items[2].price} 
+      date={props.items[2].date}/>
+    </div>
+    )
+}
+export default Expenses
