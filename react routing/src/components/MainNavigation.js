5c9d666f974b1720167f52174e2fb3c753c09741
AbuJubaeid <abujubaeidpranto2@gmail.com>
2023-12-26 16:35:38 +0600

    Add files via upload

--- react routing/src/components/MainNavigation.js
+++ react routing/src/components/MainNavigation.js
@@ -1,1 +1,22 @@
+import { NavLink } from "react-router-dom"
+import classes from './MainNavigation.module.css'
 
+
+function MainNavigation (){
+    return (
+        <header className={classes.header}>
+            <nav>
+               <ul className={classes.list}>
+                 <li >
+                    <NavLink className={({isActive})=>isActive ? classes.active : undefined} to="/" end>Home Page</NavLink>
+                 </li>
+                 <li>
+                 <NavLink className={({isActive})=>isActive ? classes.active : undefined} to="/products">Product Page</NavLink>
+                 </li> 
+               </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default MainNavigation
